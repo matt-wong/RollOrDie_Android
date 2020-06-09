e9eb317672697163ee446286aff3108cbb3880bc
Unknown <matthew.alexander.wong@gmail.com>
2020-06-08 21:26:02 -0600

    Add all Unity Files

---
+++ Assets/touchMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPos.z = 0;
+            transform.position = touchPos;
+        }
+    }
+}
