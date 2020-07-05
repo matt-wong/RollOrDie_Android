88da8d02acebb3fa05ed4f298b2c6a862ba0d879
Unknown <matthew.alexander.wong@gmail.com>
2020-07-04 22:12:26 -0600

    Fix up some of the text.

--- Assets/HudScript.cs
+++ Assets/HudScript.cs
@@ -7,8 +7,9 @@ public class HudScript : MonoBehaviour
 {
 
     Text myPointKeeper;
-    Text myHSKeeper;
     Text myDebugData;
+    Text myFinalScoreText;
+    Text myHighScoreText;
     playerScript myPlayer;
     Transform myRestartPanel;
     gameManager gm;
@@ -17,12 +18,14 @@ public class HudScript : MonoBehaviour
     void Start()
     {
      
-        this.myHSKeeper = transform.Find("highscoreText").GetComponent<Text>();
         this.myPointKeeper = transform.Find("scoreText").GetComponent<Text>();
         this.myDebugData = transform.Find("debugData").GetComponent<Text>();
+
         this.myPlayer = GameObject.FindObjectOfType<playerScript>();
 
         myRestartPanel = transform.Find("RestartPanel").GetComponent<Transform>();
+        this.myFinalScoreText = myRestartPanel.Find("FinalScoreText").GetComponent<Text>();
+        this.myHighScoreText = myRestartPanel.Find("HighScoreText").GetComponent<Text>();
         myRestartPanel.gameObject.SetActive(false);
 
         gm = gameManager.Instance;
@@ -33,10 +36,11 @@ public class HudScript : MonoBehaviour
     void Update()
     {
         myPointKeeper.text = "Points: " + gm.Points.ToString();
-        myHSKeeper.text = "High Score: " + gm.HighScore.ToString();
         myDebugData.text = "Player Lives:" + myPlayer.ExtraLives;
         if (gm.GameOver){
             myRestartPanel.gameObject.SetActive(true);
+            myFinalScoreText.text = "Points: " + gm.Points.ToString();
+            myHighScoreText.text = "High Score: " + gm.HighScore.ToString();
         }
 
     }
