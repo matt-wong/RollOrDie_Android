93087a86081b159b4e179479ec87cfb4cb0d40f8
Unknown <matthew.alexander.wong@gmail.com>
2020-07-19 11:22:49 -0600

    Add visual cue for wrapping.

---
+++ Assets/warpPipeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class warpPipeScript : MonoBehaviour
+{
+    playerScript myPlayer;
+    SpriteRenderer myLeftPipe;
+    SpriteRenderer myRightPipe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myPlayer = GameObject.FindObjectOfType<playerScript>();
+        this.myLeftPipe = transform.Find("WarpPipeL").GetComponent<SpriteRenderer>();
+        this.myRightPipe = transform.Find("WarpPipeR").GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myPlayer.CanWrap){
+            myLeftPipe.color = Color.green;
+            myRightPipe.color = Color.green;
+        }
+    }
+}
