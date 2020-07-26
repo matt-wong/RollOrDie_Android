08570254ecd050dc2c509bd1c313dfc6ecbdedd1
Unknown <matthew.alexander.wong@gmail.com>
2020-07-26 14:05:36 -0600

    Add difficulty selection.

--- Assets/mainMenuScript.cs
+++ Assets/mainMenuScript.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class mainMenuScript : MonoBehaviour
 {
-    public void PlayGame(){
+
+    const int GAME_SCENE = 1;
+    const int DIFF_SCENE = 2;
+
+    private void PlayGame(eDifficulty difficulty){
         Debug.Log("PLAY GAME");
-        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(GAME_SCENE);
+        gameManager.Instance.difficulty = difficulty;
+    }
+
+    public void PlayEasy(){
+        this.PlayGame(eDifficulty.easy);
+    }
+
+    public void PlayRegular(){
+        this.PlayGame(eDifficulty.regular);
+    }
+
+    public void ChooseDifficulty(){
+        Debug.Log("PLAY GAME");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(DIFF_SCENE);
     }
 }
