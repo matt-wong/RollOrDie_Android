066fd9c1621491dd82f42bb85d4612fabd79ffbe
Unknown <matthew.alexander.wong@gmail.com>
2020-08-19 18:10:25 -0600

    Randomize BG music!

---
+++ Assets/BackgroundMusicScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicScript : MonoBehaviour
+{
+
+    private AudioSource myAudioSource;
+    public AudioClip[] Songs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudioSource = gameObject.GetComponent<AudioSource>();
+        myAudioSource.clip = Songs[UnityEngine.Random.Range(0,this.Songs.Length)];
+        myAudioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
