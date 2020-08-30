4b9127284d22b9f7350979ed1d3d538b7ec7a67e
Unknown <matthew.alexander.wong@gmail.com>
2020-08-29 19:10:07 -0600

    Add enemy death sounds.

--- Assets/enemyScript.cs
+++ Assets/enemyScript.cs
@@ -11,6 +11,8 @@ public class enemyScript : fallingObject
     public DiceFace currFace;
     DiceFace[] faces;
 
+    public AudioClip[] DeathSounds;
+
     public Sprite[] faceSprites;
     public ParticleSystem DeathParticles;
 
@@ -32,7 +34,6 @@ public class enemyScript : fallingObject
         mySpriteRenderer.sprite = this.currFace.sprite;
 
         Animator animator = GetComponent<Animator>();
-
         animator.Play("DiceEnemy" + this.currFace.Value.ToString(), -1, Random.Range(0f, 1f));
      }
 
@@ -109,14 +110,25 @@ public class enemyScript : fallingObject
         
         ParticleSystem.MainModule settings = ps.main;
         settings.startColor = new ParticleSystem.MinMaxGradient(this.mySpriteRenderer.color);
-
         ps.Play();
+
+        AudioSource audSource  = GetComponent<AudioSource>();
+        if (audSource){
+            audSource.clip = this.DeathSounds[Random.Range(0,this.DeathSounds.Length)];
+            Debug.Log(audSource.clip.ToString());
+            audSource.time = 0.2f;
+            audSource.Play();
+        }
 
         if (DiedAction != null){
             DiedAction.Invoke();
         }
 
-        Destroy(gameObject);
+        Invoke("DestroyMe", 1);
+    }
+
+    private void DestroyMe(){
+        Destroy(gameObject);        
     }
 
     internal void Disable()
