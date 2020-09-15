4e09231d9042b5d21e7135e8fc58857e81ba196d
Unknown <matthew.alexander.wong@gmail.com>
2020-09-14 20:35:15 -0600

    Add mute button!

---
+++ Assets/MuteButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MuteButton : MonoBehaviour
+{
+
+    private TextMeshProUGUI myButtonText;
+
+    void Start()
+    {
+      myButtonText = transform.GetComponentInChildren<TextMeshProUGUI>();
+      Debug.Log(myButtonText);
+    }
+
+    public void ToggleSound()
+    {
+        gameManager.Instance.MuteMusic = !gameManager.Instance.MuteMusic;
+        Debug.Log("Mute is " + gameManager.Instance.MuteMusic);
+        if (gameManager.Instance.MuteMusic)
+        {
+            AudioListener.volume = 0;
+            myButtonText.text = "Sound is OFF";
+        }
+        else
+        {
+            AudioListener.volume = 1;
+            myButtonText.text = "Sound is ON";
+        }
+    }
+}
