93e32ac4477aa9b673d2a41bfe2a0d1ff33353b2
Unknown <matthew.alexander.wong@gmail.com>
2020-09-19 09:42:41 -0600

    Add item descriptors on Pick up!

---
+++ Assets/ItemDescriptor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ItemDescriptor : MonoBehaviour
+{
+
+    private TextMeshProUGUI mytext;
+    private float myDeathTimer;
+    const float DEATH_TIME = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mytext = GetComponent<TextMeshProUGUI>();
+        myDeathTimer = DEATH_TIME;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myDeathTimer -= Time.deltaTime;
+        mytext.faceColor = new Color(0f,0f,0f, myDeathTimer / DEATH_TIME);
+    }
+}
