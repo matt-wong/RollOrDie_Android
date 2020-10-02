2a08e31ca5ebbe2f3abec7a78d2fb9cd37dedfe6
Unknown <matthew.alexander.wong@gmail.com>
2020-10-01 19:06:35 -0600

    Create particle killer script.

---
+++ Assets/ParticleKiller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleKiller : MonoBehaviour
+{
+
+    private ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = gameObject.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (ps)
+        {
+            if (!ps.IsAlive())
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
+
