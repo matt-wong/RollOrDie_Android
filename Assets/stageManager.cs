cdecf35534a68dc52099d56a9843defc01111a94
Unknown <matthew.alexander.wong@gmail.com>
2020-10-24 13:54:21 -0600

    Whitespace.

--- Assets/stageManager.cs
+++ Assets/stageManager.cs
@@ -46,18 +46,21 @@ public class stageManager : MonoBehaviour
         this.Stages.Add(new Stage { StartingRow = 15, BgColor = BG_ORANGE, NumberOfObstacles = 0, Description = "SPEED UP!", MusicSpeed = 1.0375f}); //Faster - Orange
         this.Stages.Add(new Stage { StartingRow = 20, BgColor = BG_YELLOW, NumberOfObstacles = 1, Description = "AVOID SPIKEYS!", MusicSpeed = 1.05f}); //1 Obstacle / row - Yellow 
         this.Stages.Add(new Stage { StartingRow = 30, BgColor = BG_GREEN, NumberOfObstacles = 2, Description = "AVOID MORE SPIKEYS!"}); //2 Obstacle / row -Green
+        
         this.Stages.Add(new Stage { StartingRow = 40, BgColor = BG_BLUE, NumberOfObstacles = 3, Description = "MORE SPIKEY GUYS!"}); //3 Obstacle / row -New Blob Sprite
         this.Stages.Add(new Stage { StartingRow = 50, BgColor = BG_PURPLE, NumberOfObstacles = 4, Description = "CONTINUE AVOIDING SPIKEY GUYS!"}); //4 Obstacles
         this.Stages.Add(new Stage { StartingRow = 60, BgColor = BG_RED, NumberOfObstacles = 4, NumberBlockedDiced = 1, Description = "BLOCKED DICE!"}); // 5 Obstacles
         this.Stages.Add(new Stage { StartingRow = 70, BgColor = BG_ORANGE, NumberOfObstacles = 5, NumberBlockedDiced = 1, Description = "MORE SPIKEY GUYS!"}); // 6 Obstacles
         this.Stages.Add(new Stage { StartingRow = 80, BgColor = BG_YELLOW, NumberOfObstacles = 6, NumberBlockedDiced = 2, Description = "+1 BLOCKED + 1 SPIKE!"}); // 7 Obstacles
         this.Stages.Add(new Stage { StartingRow = 90, BgColor = BG_GREEN, NumberOfObstacles = 7, NumberBlockedDiced = 2, Description = "MORE SPIKEY GUYS!"}); // 8 Obs
+        
         this.Stages.Add(new Stage { StartingRow = 100, BgColor = BG_BLUE, NumberOfObstacles = 8, NumberBlockedDiced = 2, Description = "MORE SPIKEY GUYS!"}); 
         this.Stages.Add(new Stage { StartingRow = 110, BgColor = BG_PURPLE, NumberOfObstacles = 8, NumberBlockedDiced = 3, Description = "ANOTHER BLOCKED DICE!"}); 
         this.Stages.Add(new Stage { StartingRow = 120, BgColor = BG_RED, NumberOfObstacles = 8, NumberBlockedDiced = 4, Description = "ANOTHER BLOCKED DICE!"}); 
         this.Stages.Add(new Stage { StartingRow = 130, BgColor = BG_ORANGE, NumberOfObstacles = 8, NumberBlockedDiced = 5, Description = "ANOTHER BLOCKED DICE!"});
         this.Stages.Add(new Stage { StartingRow = 140, BgColor = BG_YELLOW, NumberOfObstacles = 9, NumberBlockedDiced = 5, Description = "ANOTHER SPIKEY GUY!"});
         this.Stages.Add(new Stage { StartingRow = 150, BgColor = BG_GREEN, NumberOfObstacles = 10, NumberBlockedDiced = 5, Description = "Please avoid them!"});
+        
         this.Stages.Add(new Stage { StartingRow = 160, BgColor = BG_BLUE, NumberOfObstacles = 11, NumberBlockedDiced = 5, Description = "Please! avoid them!"});
 
         foreach(Stage stg in this.Stages){
