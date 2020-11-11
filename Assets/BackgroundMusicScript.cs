6779e275c80e724e47f8e2a615d7f92df2c80ccf
Unknown <matthew.alexander.wong@gmail.com>
2020-11-11 09:56:37 -0700

    Single looping track for BG music.

--- Assets/BackgroundMusicScript.cs
+++ Assets/BackgroundMusicScript.cs
@@ -8,22 +8,18 @@ public class BackgroundMusicScript : MonoBehaviour
 
     private AudioSource myAudioSource;
     public AudioClip[] Songs;
-    public AudioClip[] SongLoops;
-    private stageManager stageManager;
-    private int mySongIndex = 0; 
 
     // Start is called before the first frame update
     void Start()
     {
         myAudioSource = gameObject.GetComponent<AudioSource>();
-        mySongIndex = UnityEngine.Random.Range(0,this.Songs.Length);
-        myAudioSource.clip = Songs[mySongIndex];
+        myAudioSource.clip = Songs[UnityEngine.Random.Range(0,this.Songs.Length)];
         myAudioSource.Play();
 
         GameObject canvas = GameObject.FindGameObjectWithTag("Canvas");
-        this.stageManager = canvas.GetComponent<stageManager>();
-        if (this.stageManager){
-            this.stageManager.NewStageAction += (value) => {this.AdjustToNewStage(value);};
+        stageManager stageManager = canvas.GetComponent<stageManager>();
+        if (stageManager){
+            stageManager.NewStageAction += (value) => {this.AdjustToNewStage(value);};
         }
     }
 
@@ -31,17 +27,6 @@ public class BackgroundMusicScript : MonoBehaviour
     {
         if (value.MusicSpeed > 0){ //Was set.
         this.myAudioSource.pitch = value.MusicSpeed;
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!myAudioSource.isPlaying)
-        {
-            myAudioSource.clip = SongLoops[mySongIndex];
-            myAudioSource.Play();
-            myAudioSource.loop = true;
         }
     }
 }
