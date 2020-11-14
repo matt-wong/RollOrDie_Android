1966b9dc19abcb80787cfd652296e934e28c61cf
Unknown <matthew.alexander.wong@gmail.com>
2020-11-14 12:58:46 -0700

    Fix grammar on HowToPlay page.

--- Assets/playTestersDisplay.cs
+++ Assets/playTestersDisplay.cs
@@ -8,7 +8,7 @@ public class playTestersDisplay : MonoBehaviour
 
     Text myText;
     int nameIdx = 0;
-    string[] names = {"Alicia", "Rocket Rob", "Rob 2", "my Mom, I think...", "Paullison (2)", "Scott", "Geoff with a G", "Alicia's Brothers", "Kevin"};
+    string[] names = {"Alicia", "Rocket Rob", "Rob 2", "my Mom, I think...", "Paullison (2)", "Scott", "Geoff with a G", "Alicia's Brothers", "Kevin", "Other Matt"};
 
     // Start is called before the first frame update
     void Start()
