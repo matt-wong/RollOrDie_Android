c01ab58ce5ee2476286bbcb85b84e16c0506ad25
Unknown <matthew.alexander.wong@gmail.com>
2020-11-13 20:18:22 -0700

    Add playtesters credits

---
+++ Assets/playTestersDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playTestersDisplay : MonoBehaviour
+{
+
+    Text myText;
+    int nameIdx = 0;
+    string[] names = {"Alicia", "Rocket Rob", "Rob 2", "my Mom, I think...", "Paullison (2)", "Scott", "Geoff with a G", "Alicia's Brothers", "Kevin"};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myText = Transform.FindObjectOfType<Text>();
+        InvokeRepeating("ChangeName", 0f, 2f);
+    }
+
+    void ChangeName(){
+        if (myText){
+            myText.text = names[nameIdx % names.Length];
+            nameIdx += 1;
+        }
+    }
+}
