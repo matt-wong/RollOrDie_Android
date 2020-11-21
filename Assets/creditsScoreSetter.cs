d193573d8b8419edcde4f168ab7691d305f4b8da
Unknown <matthew.alexander.wong@gmail.com>
2020-11-21 12:57:16 -0700

    Add points to credits page

---
+++ Assets/creditsScoreSetter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class creditsScoreSetter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        scoreText.text = "Score: " + gameManager.Instance.Points;
+
+        Text highScoreText = GameObject.Find("HighScoreText").GetComponent<Text>();
+        highScoreText.text = "High Score: " + gameManager.Instance.HighScore;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
